45becb9713ccbce12d09e74b2617c789bdc6a3ea
vadimshulga <stiffler8888@gmail.com>
2022-01-31 15:14:58 +0300

    edit iOS locators

--- src/test/java/IOSCalendarTest.java
+++ src/test/java/IOSCalendarTest.java
@@ -32,28 +32,28 @@ public class IOSCalendarTest {
 
     @Test
     public void createEvent() {
-        driver.findElement(MobileBy.iOSClassChain("**/XCUIElementTypeButton[`label == \"Today\"`]")).click();
-        driver.findElement(MobileBy.iOSNsPredicateString("label == \"Add\"")).click();
-        driver.findElement(MobileBy.iOSNsPredicateString("name == \"Title\"")).sendKeys(testEvent.getEventName());
+        driver.findElement(MobileBy.iOSClassChain("**/XCUIElementTypeButton[`label == 'Today'`]")).click();
+        driver.findElement(MobileBy.iOSNsPredicateString("label == 'Add'")).click();
+        driver.findElement(MobileBy.iOSNsPredicateString("name == 'Title'")).sendKeys(testEvent.getEventName());
 
         //enter start date
-        driver.findElement(MobileBy.iOSClassChain("**/XCUIElementTypeOther[`label == \"Date and Time Picker\"`][1]/XCUIElementTypeButton[2]")).click();
+        driver.findElement(MobileBy.iOSClassChain("**/XCUIElementTypeOther[`label == 'Date and Time Picker'`][1]/XCUIElementTypeButton[2]")).click();
         List<MobileElement> wheelsStart = driver.findElements(MobileBy.iOSClassChain("**/XCUIElementTypePickerWheel"));
         wheelsStart.get(0).sendKeys(testEvent.getStartHours());
         wheelsStart.get(1).sendKeys(testEvent.getIosStartMin());
         wheelsStart.get(2).sendKeys(testEvent.getStartDayTime());
 
         //enter end date
-        driver.findElement(MobileBy.iOSClassChain("**/XCUIElementTypeOther[`label == \"Date and Time Picker\"`][2]/XCUIElementTypeButton[2]")).click();
+        driver.findElement(MobileBy.iOSClassChain("**/XCUIElementTypeOther[`label == 'Date and Time Picker'`][2]/XCUIElementTypeButton[2]")).click();
         List<MobileElement> wheelsEnd = driver.findElements(MobileBy.iOSClassChain("**/XCUIElementTypePickerWheel"));
         wheelsEnd.get(0).sendKeys(testEvent.getEndHours());
         wheelsEnd.get(1).sendKeys(testEvent.getIosEndMin());
         wheelsEnd.get(2).sendKeys(testEvent.getEndDayTime());
-        driver.findElement(MobileBy.iOSNsPredicateString("label == \"Add\" AND visible == 1")).click();
+        driver.findElement(MobileBy.iOSNsPredicateString("label == 'Add' AND visible == 1")).click();
 
         //validation
-        Assert.assertTrue("The event is missed in the list of events", !driver.findElements(MobileBy.iOSClassChain(String.format("**/XCUIElementTypeButton[`name CONTAINS[cd] \"%s\"`]", testEvent.getEventName()))).isEmpty());
-        String expectedDetails = driver.findElement(MobileBy.iOSClassChain(String.format("**/XCUIElementTypeButton[`name CONTAINS[cd] \"%s\"`]", testEvent.getEventName()))).getText();
+        Assert.assertTrue("The event is missed in the list of events", !driver.findElements(MobileBy.iOSClassChain(String.format("**/XCUIElementTypeButton[`name CONTAINS[cd] '%s'`]", testEvent.getEventName()))).isEmpty());
+        String expectedDetails = driver.findElement(MobileBy.iOSClassChain(String.format("**/XCUIElementTypeButton[`name CONTAINS[cd] '%s'`]", testEvent.getEventName()))).getText();
         String actualDetails = testEvent.getIosEventDetails();
 
         Assert.assertEquals("Expected event details don't match to actual ones", expectedDetails, actualDetails);
@@ -63,14 +63,14 @@ public class IOSCalendarTest {
     public void deleteEvent() {
         driver.closeApp();
         driver.launchApp();
-        if (!driver.findElements(MobileBy.iOSClassChain(String.format("**/XCUIElementTypeButton[`name CONTAINS[cd] \"%s\"`]", testEvent.getEventName()))).isEmpty()) {
+        if (!driver.findElements(MobileBy.iOSClassChain(String.format("**/XCUIElementTypeButton[`name CONTAINS[cd] '%s'`]", testEvent.getEventName()))).isEmpty()) {
             HashMap<String, String> scrollObject = new HashMap<>();
             scrollObject.put("direction", "up");
             scrollObject.put("index", "13");
             driver.executeScript("mobile: swipe", scrollObject);
-            driver.findElement(MobileBy.iOSClassChain(String.format("**/XCUIElementTypeButton[`name CONTAINS[cd] \"%s\"`]", testEvent.getEventName()))).click();
-            driver.findElement(MobileBy.iOSNsPredicateString("value == \"Delete Event\"")).click();
-            driver.findElement(MobileBy.iOSNsPredicateString("label == \"Delete Event\" AND visible = 1")).click();
+            driver.findElement(MobileBy.iOSClassChain(String.format("**/XCUIElementTypeButton[`name CONTAINS[cd] '%s'`]", testEvent.getEventName()))).click();
+            driver.findElement(MobileBy.iOSNsPredicateString("value == 'Delete Event'")).click();
+            driver.findElement(MobileBy.iOSNsPredicateString("label == 'Delete Event' AND visible = 1")).click();
         }
     }
 
